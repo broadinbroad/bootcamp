69a06e76af6ec7cb43c3504fa86edb715fbe3952
Justin.Bois <hklumpe@caltech.edu>
2016-09-13 15:51:04 -0700

    Added ECDF function to bootcamp_utils

---
+++ bootcamp_utils.py
@@ -0,0 +1,23 @@
+# Bootcamp Utilities:
+"""A collection of statistical functions proved
+useful for 55 students."""
+
+
+# Import modules needed
+import numpy as np
+
+# Define function for ecdf
+def ecdf(data):
+    """
+    Compute x, y values for an empirical cumulative distribtuion
+    function
+    """
+
+    # Sort the data
+    x = np.sort(data)
+
+    # each data point is 1/n_pts fraction of data
+    # use to computed ecdf
+    y = np.arange(1, len(data)+1) / len(data)
+
+    return x, y
